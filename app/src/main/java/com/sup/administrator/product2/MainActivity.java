476db45e5d4389af6937b140712104296ea7eb9e
unknown <silpasupa@gmail.com>
2019-01-30 14:32:48 +0530

    first commit

---
+++ app/src/main/java/com/sup/administrator/product2/MainActivity.java
@@ -0,0 +1,59 @@
+package com.sup.administrator.product2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText e1,e2,e3,e4,e5,e6,e7;
+    Button b;
+    String s1,s2,s3,s4,s5,s6,s7;
+    pddb p;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.mod);
+        e2=(EditText)findViewById(R.id.cod);
+        e3=(EditText)findViewById(R.id.nam);
+        e4=(EditText)findViewById(R.id.sel);
+        e5=(EditText)findViewById(R.id.pri);
+        e6=(EditText)findViewById(R.id.oname);
+        e7=(EditText)findViewById(R.id.mob);
+        b=(Button)findViewById(R.id.sub);
+        p=new pddb(this);
+        p.getWritableDatabase();
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1=e1.getText().toString();
+                s2=e2.getText().toString();
+                s3=e3.getText().toString();
+                s4=e4.getText().toString();
+                s5=e5.getText().toString();
+                s6=e6.getText().toString();
+                s7=e7.getText().toString();
+                Log.d("mod",s1);
+                Log.d("cod",s2);
+                Log.d("nam",s3);
+                Log.d("sel",s4);
+                Log.d("pri",s5);
+                Log.d("on",s6);
+                Log.d("mob",s7);
+                boolean status=p.insert(s1,s2,s3,s4,s5,s6,s7);
+                if(status==true)
+                {
+                    Toast.makeText(getApplicationContext(),"success",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"error",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/sup/administrator/product2/pddb.java
@@ -0,0 +1,60 @@
+package com.sup.administrator.product2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 1/30/2019.
+ */
+public class pddb extends SQLiteOpenHelper {
+    public static final String dbname="product.db";
+    public static final String tablename="product";
+    public static final String col1="id";
+    public static final String col2="mod";
+    public static final String col3="cod";
+    public static final String col4="nam";
+    public static final String col5="sel";
+    public static final String col6="pri";
+    public static final String col7="oname";
+    public static final String col8="mob";
+    public pddb(Context context) {
+        super(context, dbname, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+ String query="create table "+tablename+"("+col1+ " integer primary key autoincrement, "+col2+ " text, "+col3+ " text, "+col4+ " text, "+col5+ " text, "+col6+ " text, "+col7+ " text, "+col8+ " text )";
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String query="drop table if exists "+tablename;
+        sqLiteDatabase.execSQL(query);
+        onCreate(sqLiteDatabase);
+
+    }
+    public boolean insert(String mod,String  cod,String nam,String sel,String pri,String oname,String mob)
+    {
+        SQLiteDatabase sqLiteDatabase=this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put(col2,mod);
+        cv.put(col3,cod);
+        cv.put(col4,nam);
+        cv.put(col5,sel);
+        cv.put(col6,pri);
+        cv.put(col7,oname);
+        cv.put(col8,mob);
+        Long status=sqLiteDatabase.insert(tablename,null,cv);
+        if(status==-1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
